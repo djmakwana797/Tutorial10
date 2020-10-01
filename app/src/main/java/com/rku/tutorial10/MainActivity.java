1c418e65a445b4388ee006af232e00fbce5cabf0
djmakwana797 <dmakwana979@rku.ac.in>
2020-10-01 21:27:09 +0530

    parsing local json file with AsyncTask.

--- app/src/main/java/com/rku/tutorial10/MainActivity.java
+++ app/src/main/java/com/rku/tutorial10/MainActivity.java
@@ -2,6 +2,8 @@ package com.rku.tutorial10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Adapter;
@@ -19,35 +21,58 @@ public class MainActivity extends AppCompatActivity {
 
     ListView lstData;
     CustomAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         lstData = findViewById(R.id.lstData);
-        adapter = new CustomAdapter(this,generateData());
-        lstData.setAdapter(adapter);
+
+        new MyAsyncTask().execute();
     }
 
-    private JSONArray generateData() {
+    class MyAsyncTask extends AsyncTask {
+        ProgressDialog dialog;
         JSONObject jsonObject;
         JSONArray jsonArray = null;
-        try {
-            InputStream is = getAssets().open("data.json");
-            int size = 0;
-            size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            String json = new String(buffer,"UTF-8");
-            Log.i("json",json);
 
-            jsonObject = new JSONObject(json);
-            jsonArray = jsonObject.getJSONArray("sites");
-
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.show();
         }
-        return jsonArray;
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try {
+                InputStream is = getAssets().open("data.json");
+                int size = 0;
+                size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+                String json = new String(buffer,"UTF-8");
+                Log.i("json",json);
+
+                jsonObject = new JSONObject(json);
+                jsonArray = jsonObject.getJSONArray("sites");
+
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            adapter = new CustomAdapter(MainActivity.this,jsonArray);
+            lstData.setAdapter(adapter);
+
+            if(dialog.isShowing())dialog.dismiss();
+        }
     }
 }
