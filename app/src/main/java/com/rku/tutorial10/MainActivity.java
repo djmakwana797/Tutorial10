db60b2b41afd96fecd91da49387e3d3b123559a6
djmakwana797 <dmakwana979@rku.ac.in>
2020-10-01 20:43:57 +0530

    local json parsing works.

--- app/src/main/java/com/rku/tutorial10/MainActivity.java
+++ app/src/main/java/com/rku/tutorial10/MainActivity.java
@@ -3,12 +3,51 @@ package com.rku.tutorial10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lstData;
+    CustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lstData = findViewById(R.id.lstData);
+        adapter = new CustomAdapter(this,generateData());
+        lstData.setAdapter(adapter);
+    }
+
+    private JSONArray generateData() {
+        JSONObject jsonObject;
+        JSONArray jsonArray = null;
+        try {
+            InputStream is = getAssets().open("data.json");
+            int size = 0;
+            size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer,"UTF-8");
+            Log.i("json",json);
+
+            jsonObject = new JSONObject(json);
+            jsonArray = jsonObject.getJSONArray("sites");
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
     }
 }
